617f4effa2888541ea33f767fb224404524c957a
Tang Nakry <nxasenpai007@gmail.com>
2025-11-16 14:30:51 +0700

    Initial Committ

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
